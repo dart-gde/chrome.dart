431707f7dd41cc8ee911cc6681b703d816cbc403
adam <financeCoding@gmail.com>
2012-12-18 12:18:29 -0800

    Adding formatted logging

--- test/harness_browser.dart
+++ test/harness_browser.dart
@@ -1,5 +1,7 @@
 library harness_browser;
 
+import 'dart:html' as html;
+
 import 'package:unittest/html_enhanced_config.dart';
 import 'package:unittest/unittest.dart';
 import 'package:logging/logging.dart';
@@ -10,11 +12,30 @@ import 'package:chrome/chrome.dart';
 part 'runtime_test.dart';
 
 main() {
-  Logger.root.level = Level.ALL;
-  Logger.root.on.record.add((LogRecord r)=>print(r.message.toString()));
-
+  Logger.root.level = Level.ALL; 
+  Logger.root.on.record.add((LogRecord r) {
+    StringBuffer sb = new StringBuffer();
+    sb
+    ..add(r.time.toString())
+    ..add(":")
+    ..add(r.loggerName)
+    ..add(":")
+    ..add(r.level.name)
+    ..add(":")
+    ..add(r.sequenceNumber)
+    ..add(": ")
+    ..add(r.message.toString());
+    print(sb.toString()); 
+  });
+    
   groupSep = '.';
   useHtmlEnhancedConfiguration();
 
+  html.window.on.keyUp.add((html.KeyboardEvent event) {
+    if (event.keyCode == html.KeyCode.R) {
+      new Runtime().reload();
+    }
+  });
+  
   new RuntimeTest().main();
 }
