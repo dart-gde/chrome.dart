a4555c1559faf2a7dc47ee04c06ec53e4656137c
adam <financeCoding@gmail.com>
2013-05-19 10:57:13 -0700

    Adding more hop tasks

--- tool/hop_runner.dart
+++ tool/hop_runner.dart
@@ -6,18 +6,41 @@ void main() {
       packageRoot: 'packages/'
   ));
 
-  addTask('example_serial', createDart2JsTask(['example/serial_example/web/chrome_app_serial_example.dart'],
+  addTask('serial_example', createDart2JsTask(
+      ['example/serial_example/web/chrome_app_serial_example.dart'],
+      allowUnsafeEval: false,
+      packageRoot: 'packages/'
+  ));
+
+  addTask('serial_clock', createDart2JsTask(
+      ['example/serial_clock/web/chrome_app_serial_example.dart'],
+      allowUnsafeEval: false,
+      packageRoot: 'packages/'
+  ));
+
+  addTask('tcp_echo_server', createDart2JsTask(
+      ['example/tcp_echo_server/web/tcp_echo_server_example.dart'],
+      allowUnsafeEval: false,
+      packageRoot: 'packages/'
+  ));
+
+  addTask('udp_echo_server', createDart2JsTask(
+      ['example/udp_echo_server/web/udp_echo_server_example.dart'],
       allowUnsafeEval: false,
       packageRoot: 'packages/'
   ));
 
   addTask('analyze_libs', createDartAnalyzerTask(['lib/chrome.dart']));
-  addTask('analyze_tests', createDartAnalyzerTask(['test/harness_browser.dart']));
-  addTask('analyze_examples', createDartAnalyzerTask(['example/serial_clock/web/clock.dart',
-                                                      'example/serial_example/web/chrome_app_serial_example.dart',
-                                                      'example/tcp_echo_server/web/tcp_echo_server_example.dart',
-                                                      'example/udp_echo_client/web/udp_echo_client_example.dart'
-                                                      ]));
+
+  addTask('analyze_tests', createDartAnalyzerTask(
+      ['test/harness_browser.dart']));
+
+  addTask('analyze_examples', createDartAnalyzerTask(
+      ['example/serial_clock/web/clock.dart',
+       'example/serial_example/web/chrome_app_serial_example.dart',
+       'example/tcp_echo_server/web/tcp_echo_server_example.dart',
+       'example/udp_echo_client/web/udp_echo_client_example.dart'
+       ]));
 
   runHop();
 }
