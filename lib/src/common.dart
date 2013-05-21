9309bedbc3a790b39c65968f3e63fe5e28452b87
devoncarew@gmail.com <devoncarew@gmail.com>
2013-05-21 13:19:26 -0700

    added tests for app.window and storage; added test placeholder for fileSystem

--- lib/src/common.dart
+++ lib/src/common.dart
@@ -18,33 +18,6 @@ String get lastError {
   }
 }
 
-String toTitleCase(String str) {
-  if (str.length < 2) {
-    return str.toUpperCase();
-  } else {
-    return str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
-  }
-}
-
-/**
- * Strip off one set of leading and trailing single or double quotes.
- */
-String stripQuotes(String str) {
-  if (str.length < 2) {
-    return str;
-  }
-
-  if (str.startsWith("'") && str.endsWith("'")) {
-    return str.substring(1, str.length - 1);
-  }
-
-  if (str.startsWith('"') && str.endsWith('"')) {
-    return str.substring(1, str.length - 1);
-  }
-
-  return str;
-}
-
 bool isLinux() {
   return _platform().indexOf('linux') != -1;
 }
