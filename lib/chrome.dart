fff4ee0b8dbda3173ba40c58acb8f3d58da12daa
rmsmith <ross.dart.lang@gmail.com>
2013-06-27 22:37:38 +0300

    chrome.dart : remove extraneous imports, the exports do all necessary work
    since this file does not need any of the imported types (there is no
    code here) those imports do nothing; I think this makes the file a bit
    easier on the eyes :)

--- lib/chrome.dart
+++ lib/chrome.dart
@@ -1,28 +1,11 @@
 library chrome;
 
-import 'src/app.dart';
 export 'src/app.dart';
-
-import 'src/file_system.dart';
 export 'src/file_system.dart';
-
-import 'src/i18n.dart';
 export 'src/i18n.dart';
-
-import 'src/power.dart';
 export 'src/power.dart';
-
-import 'src/push_messaging.dart';
 export 'src/push_messaging.dart';
-
-import 'src/runtime.dart';
 export 'src/runtime.dart';
-
-import 'src/serial.dart';
 export 'src/serial.dart';
-
-import 'src/socket.dart';
 export 'src/socket.dart';
-
-import 'src/storage.dart';
 export 'src/storage.dart';
