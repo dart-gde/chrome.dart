a60c99ec4036bd367568a9049d335f5f4afe093d
Marc Fisher <fisherii@google.com>
2013-07-09 15:08:27 -0700

    Added chrome.tabs APIs.

--- lib/src/windows.dart
+++ lib/src/windows.dart
@@ -5,6 +5,7 @@ import 'dart:async';
 import 'package:js/js.dart' as js;
 
 import 'common.dart';
+import 'tabs.dart';
 
 /**
  * @param window Details of the window that was created.
@@ -338,17 +339,20 @@ class Window {
     return _window.height as int;
   }
 
-// TODO(DrMarcII): Uncomment when chrome.tabs api is implemented.
-//  List<Tab> get tabs {
-//    dynamic jsTabs = _window.tabs;
-//    List<Tab> tabs = [];
-//    if (jsTabs != null) {
-//      for (var i = 0; i < jsTabs.length; i++) {
-//        tabs.add(new Tab(jsTabs[i]));
-//      }
-//    }
-//    return tabs;
-//  }
+
+  /**
+   * The current tabs in the window.
+   */
+  List<Tab> get tabs {
+    dynamic jsTabs = _window.tabs;
+    List<Tab> tabs = [];
+    if (jsTabs != null) {
+      for (var i = 0; i < jsTabs.length; i++) {
+        tabs.add(new Tab(jsTabs[i]));
+      }
+    }
+    return tabs;
+  }
 
   /**
    * Whether the window is incognito.
