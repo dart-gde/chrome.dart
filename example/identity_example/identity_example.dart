11cbfc1cd7212cb95abe5e919ee49bf69eaa19ea
Adam Singer <financeCoding@gmail.com>
2013-08-02 23:52:42 -0700

    Minor updates to example

--- example/identity_example/identity_example.dart
+++ example/identity_example/identity_example.dart
@@ -3,5 +3,12 @@ import 'package:logging/logging.dart';
 import 'package:chrome/chrome.dart';
 // Example application: https://github.com/GoogleChrome/chrome-app-samples/blob/master/identity/identity.js
 void main() {
-  chromeIdentity.getAuthToken(true);
+  //chromeIdentity.getAuthToken(true);
+
+  ButtonElement signinButton = query("#signin");
+  ButtonElement revokeButton = query("#revoke");
+  signinButton.style.display = null;
+  signinButton.onClick.listen((data) {
+    chromeIdentity.getAuthToken(interactive: true).then((token) => print("token = $token"));
+  });
 }
