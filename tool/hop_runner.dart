c348a87078e8771b5e3e74b346c394420d207977
Kevin Moore <kevin@thinkpixellab.com>
2013-08-08 12:39:31 -0500

    hop_runner reorg

--- tool/hop_runner.dart
+++ tool/hop_runner.dart
@@ -1,70 +1,35 @@
 library hop_runner;
 
+import 'dart:async';
 import 'dart:io';
 import 'package:hop/hop.dart';
 import 'package:hop/hop_tasks.dart';
 
-final List<String> allTasks = new List<String>();
-final List<String> allUpdateTasks = new List<String>();
-final List<String> allAnalyzeTasks = new List<String>();
-final List<String> allBuildTasks = new List<String>();
-
-void copy(String fileName, String sourceDirectory, String destinationDirectory) {
-  var srcFile = new File('$sourceDirectory/$fileName').readAsStringSync();
-  new File('$destinationDirectory/$fileName').writeAsStringSync(srcFile);
-}
-
-Task createUpdateJSTask(String directory) =>
-    new Task.async((TaskContext context){
-      copy('dart.js', 'packages/browser', directory);
-      copy('interop.js', 'packages/browser', directory);
-      copy('dart_interop.js', 'packages/js', directory);
-      return true;
-    }, description: 'update js files from packages');
-
-
-void buildTasks(String name, String directory, List<String> filenames) {
-  final file = filenames.map((filename) => '${directory}/${filename}');
-
-  final updateTaskName = 'update_js_$name';
-  final analyzeTaskName = 'analyze_$name';
-  final buildTaskName = 'build_$name';
-
-  addTask(updateTaskName, createUpdateJSTask(directory));
-  addTask(analyzeTaskName, createAnalyzerTask(file));
-  addTask(buildTaskName, createDartCompilerTask(file, allowUnsafeEval: false));
-
-  allTasks.addAll([updateTaskName, analyzeTaskName, buildTaskName]);
-  allUpdateTasks.add(updateTaskName);
-  allAnalyzeTasks.add(analyzeTaskName);
-  allBuildTasks.add(buildTaskName);
-}
-
 void main() {
-  buildTasks('serial_example', 'example/serial_example/web',
+  _buildTasks('serial_example', 'example/serial_example/web',
       ['chrome_app_serial_example.dart']);
 
-  buildTasks('serial_clock', 'example/serial_clock/web', ['clock.dart']);
+  _buildTasks('serial_clock', 'example/serial_clock/web', ['clock.dart']);
 
-  buildTasks('tcp_echo_server', 'example/tcp_echo_server/web',
+  _buildTasks('tcp_echo_server', 'example/tcp_echo_server/web',
       ['tcp_echo_server_example.dart']);
 
-  buildTasks('udp_echo_client', 'example/udp_echo_client/web',
+  _buildTasks('udp_echo_client', 'example/udp_echo_client/web',
       ['udp_echo_client_example.dart']);
 
-  buildTasks('usb_example', 'example/usb_example',
+  _buildTasks('usb_example', 'example/usb_example',
       ['chrome_app_usb_example.dart']);
 
-  buildTasks('bluetooth_example', 'example/bluetooth_getdevices',
+  _buildTasks('bluetooth_example', 'example/bluetooth_getdevices',
       ['bluetooth_getdevices.dart']);
 
-  buildTasks('identity_example', 'example/identity_example',
+  _buildTasks('identity_example', 'example/identity_example',
   ['identity_example.dart']);
 
-  buildTasks('test_harness', 'test/app',
+  _buildTasks('test_harness', 'test/app',
       ['harness_browser.dart', 'background.dart']);
 
-  buildTasks('test_harness_extension', 'test/ext', ['harness_extension.dart']);
+  _buildTasks('test_harness_extension', 'test/ext', ['harness_extension.dart']);
 
   addChainedTask("build_and_analyze_all", allTasks,
       description: "Build and analyze all samples and tests");
@@ -83,3 +48,51 @@ void main() {
 
   runHop();
 }
+
+final List<String> allTasks = new List<String>();
+final List<String> allUpdateTasks = new List<String>();
+final List<String> allAnalyzeTasks = new List<String>();
+final List<String> allBuildTasks = new List<String>();
+
+void _buildTasks(String name, String directory, List<String> filenames) {
+  final file = filenames
+      .map((filename) => '${directory}/${filename}')
+      .toList(growable: false);
+
+  final updateTaskName = 'update_js_$name';
+  final analyzeTaskName = 'analyze_$name';
+  final buildTaskName = 'build_$name';
+
+  addTask(updateTaskName, _createUpdateJSTask(directory));
+  addTask(analyzeTaskName, createAnalyzerTask(file));
+  addTask(buildTaskName, createDartCompilerTask(file, allowUnsafeEval: false));
+
+  allTasks.addAll([updateTaskName, analyzeTaskName, buildTaskName]);
+  allUpdateTasks.add(updateTaskName);
+  allAnalyzeTasks.add(analyzeTaskName);
+  allBuildTasks.add(buildTaskName);
+}
+
+Task _createUpdateJSTask(String directory) =>
+    new Task.async((TaskContext context) {
+      var files = {
+                   'dart.js': 'packages/browser',
+                   'interop.js': 'packages/browser',
+                   'dart_interop.js': 'packages/js'
+      };
+
+      return Future
+          .forEach(files.keys, (String key) {
+            return _copy(key, files[key], directory);
+          })
+          .then((_) => true);
+
+    }, description: 'update js files from packages');
+
+Future _copy(String fileName, String sourceDirectory, String destinationDirectory) {
+  var srcFile = new File('$sourceDirectory/$fileName').readAsStringSync();
+  return new File('$destinationDirectory/$fileName')
+    .writeAsString(srcFile)
+    .then((_) => null);
+}
+
