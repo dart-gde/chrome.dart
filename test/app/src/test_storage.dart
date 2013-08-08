74be9a631c2b3976f1d25851956d1927022f1a5a
Kevin Moore <kevin@thinkpixellab.com>
2013-08-08 14:25:38 -0500

    app tests no longer crash chrome, storage tests now expose bug

--- test/app/src/test_storage.dart
+++ test/app/src/test_storage.dart
@@ -1,31 +1,35 @@
-part of harness_browser;
+library test_storage;
 
-class TestStorage {
-  void main() {
-    group('chrome.storage', () {
-      // local
-      test('local.QUOTA_BYTES', () {
-        expect(storage.local.QUOTA_BYTES, greaterThan(5000000));
-      });
+import 'dart:async';
 
-      // sync
-      test('sync.QUOTA_BYTES', () {
-        expect(storage.sync.QUOTA_BYTES, greaterThan(100000));
-      });
-      test('sync.MAX_ITEMS', () {
-        expect(storage.sync.MAX_ITEMS, greaterThan(500));
-      });
+import 'package:unittest/unittest.dart';
 
-      // get, set
-      test('local.set_get', () {
-        Future future = storage.local.set({"foo": "bar"}).then((_) {
-          storage.local.get(["foo"]).then((Map<String, String> result) {
+import 'package:chrome/src/storage.dart';
+
+void main() {
+  group('chrome.storage', () {
+    // local
+    test('local.QUOTA_BYTES', () {
+      expect(storage.local.QUOTA_BYTES, greaterThan(5000000));
+    });
+
+    // sync
+    test('sync.QUOTA_BYTES', () {
+      expect(storage.sync.QUOTA_BYTES, greaterThan(100000));
+    });
+    test('sync.MAX_ITEMS', () {
+      expect(storage.sync.MAX_ITEMS, greaterThan(500));
+    });
+
+    // get, set
+    test('local.set_get', () {
+      return storage.local.set({"foo": "bar"})
+          .then((_) {
+            return storage.local.get(["foo"]);
+          })
+          .then((Map<String, String> result) {
             expect(result["foo"], equals("bar"));
           });
-        });
-
-        expect(future, completes);
-      });
     });
-  }
+  });
 }
