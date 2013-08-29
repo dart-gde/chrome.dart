ad4ae2d4285d3211982c32bede5ba67995b74eb9
devoncarew@gmail.com <devoncarew@gmail.com>
2013-08-29 09:52:30 -0700

    redux of the files-test branch

--- lib/src/file_system.dart
+++ lib/src/file_system.dart
@@ -1,8 +1,6 @@
 library chrome.file_system;
 
 import 'dart:async';
-import 'dart:html';
-import 'dart:typed_data';
 
 import 'package:js/js.dart' as js;
 import 'package:meta/meta.dart';
@@ -10,6 +8,9 @@ import 'package:meta/meta.dart';
 import 'common.dart';
 
 /// Accessor for the `chrome.fileSystem` namespace.
+///
+/// Additional documentation is available here:
+///   http://developer.chrome.com/apps/fileSystem.html.
 final ChromeFileSystem fileSystem  = new ChromeFileSystem._();
 
 /**
@@ -28,8 +29,10 @@ class ChromeFileSystem {
    * Get the display path of a FileEntry object. The display path is based on
    * the full path of the file on the local file system, but may be made more
    * readable for display purposes.
+   *
+   * [fileEntry] should be a dom FileEntry
    */
-  Future<String> getDisplayPath(FileEntry fileEntry) {
+  Future<String> getDisplayPath(js.Proxy fileEntry) {
     ChromeCompleter<String> completer = new ChromeCompleter.oneArg();
     _fileSystem.getDisplayPath(fileEntry, completer.callback);
     return completer.future;
@@ -39,11 +42,13 @@ class ChromeFileSystem {
    * Get a writable FileEntry from another FileEntry. This call will fail if the
    * application does not have the 'write' permission under 'fileSystem'.
    *
+   * [fileEntry] should be a dom FileEntry
+   *
    * Note that this will soon be deprecated.
    */
   @deprecated
-  Future<FileEntry> getWritableEntry(FileEntry fileEntry) {
-    ChromeCompleter<FileEntry> completer = new ChromeCompleter.oneArg();
+  Future<js.Proxy> getWritableEntry(js.Proxy fileEntry) {
+    ChromeCompleter<js.Proxy> completer = new ChromeCompleter.oneArg(js.retain);
     _fileSystem.getWritableEntry(fileEntry, completer.callback);
     return completer.future;
   }
@@ -51,10 +56,12 @@ class ChromeFileSystem {
   /**
    * Gets whether this FileEntry is writable or not.
    *
+   * [fileEntry] should be a dom FileEntry
+   *
    * Note that this will soon be deprecated.
    */
   @deprecated
-  Future<bool> isWritableEntry(FileEntry fileEntry) {
+  Future<bool> isWritableEntry(js.Proxy fileEntry) {
     ChromeCompleter<bool> completer = new ChromeCompleter.oneArg();
     _fileSystem.isWritableEntry(fileEntry, completer.callback);
     return completer.future;
@@ -65,8 +72,11 @@ class ChromeFileSystem {
    *
    * [type] is one of 'openFile', 'openWritableFile', 'saveFile'. Note that
    * 'openWritableFile' will soon be deprecated.
+   *
+   * This will return a dom FileEntry. js.retain() has been called on it; it is
+   * the caller's responsibility to call js.release();
    */
-  Future<FileEntry> chooseEntry({
+  Future<js.Proxy> chooseEntry({
     String type: 'openFile',
     String suggestedName,
     List<ChooseEntryAccepts> accepts,
@@ -75,11 +85,11 @@ class ChromeFileSystem {
     Map<String, dynamic> options = {};
     if (type != null) options['type'] = type;
     if (suggestedName != null) options['suggestedName'] = suggestedName;
-    if (accepts != null) options['openFile'] = js.array(accepts);
+    if (accepts != null) options['accepts'] = js.array(accepts);
     if (acceptsAllTypes != null) options['acceptsAllTypes'] = acceptsAllTypes;
 
-    ChromeCompleter<FileEntry> completer = new ChromeCompleter.twoArgs(
-        (fileEntry, fileEntries) => fileEntry);
+    ChromeCompleter<js.Proxy> completer = new ChromeCompleter.twoArgs(
+        (fileEntry, fileEntries) => js.retain(fileEntry));
     _fileSystem.chooseEntry(js.map(options), completer.callback);
     return completer.future;
   }
@@ -87,8 +97,11 @@ class ChromeFileSystem {
   /**
    * Ask the user to choose (open) a directory. This is sugar for the
    * chrome.fileSystem.chooseEntry call.
+   *
+   * This will return a dom DirectoryEntry. js.retain() has been called on it;
+   * it is the caller's responsibility to call js.release();
    */
-  Future<DirectoryEntry> chooseEntryDirectory({
+  Future<js.Proxy> chooseEntryDirectory({
     String suggestedName,
     List<ChooseEntryAccepts> accepts,
     bool acceptsAllTypes: true}) {
@@ -96,11 +109,11 @@ class ChromeFileSystem {
     Map<String, dynamic> options = {};
     options['type'] = 'openDirectory';
     if (suggestedName != null) options['suggestedName'] = suggestedName;
-    if (accepts != null) options['openFile'] = js.array(accepts);
+    if (accepts != null) options['accepts'] = js.array(accepts);
     if (acceptsAllTypes != null) options['acceptsAllTypes'] = acceptsAllTypes;
 
-    ChromeCompleter<DirectoryEntry> completer = new ChromeCompleter.twoArgs(
-        (fileEntry, fileEntries) => fileEntry);
+    ChromeCompleter<js.Proxy> completer = new ChromeCompleter.twoArgs(
+        (fileEntry, fileEntries) => js.retain(fileEntry));
     _fileSystem.chooseEntry(js.map(options), completer.callback);
     return completer.future;
   }
@@ -110,8 +123,10 @@ class ChromeFileSystem {
    * chrome.fileSystem.chooseEntry call.
    *
    * [type] is one of "openFile", "openWritableFile", or "saveFile"
+   *
+   * This will return a list of dom FileEntry objects.
    */
-  Future<List<FileEntry>> chooseEntries({
+  Future<List<js.Proxy>> chooseEntries({
     String type: 'openFile',
     String suggestedName,
     List<ChooseEntryAccepts> accepts,
@@ -120,15 +135,15 @@ class ChromeFileSystem {
     Map<String, dynamic> options = {};
     if (type != null) options['type'] = type;
     if (suggestedName != null) options['suggestedName'] = suggestedName;
-    if (accepts != null) options['openFile'] = js.array(accepts);
+    if (accepts != null) options['accepts'] = js.array(accepts);
     if (acceptsAllTypes != null) options['acceptsAllTypes'] = acceptsAllTypes;
     options['acceptsMultiple'] = true;
 
-    ChromeCompleter<List<FileEntry>> completer = new ChromeCompleter.twoArgs((fileEntry, fileEntries) {
+    ChromeCompleter<List<js.Proxy>> completer = new ChromeCompleter.twoArgs((fileEntry, fileEntries) {
       if (fileEntries != null) {
-        return fileEntries;
+        return js.retain(fileEntries);
       } else if (fileEntry != null) {
-        return [fileEntry];
+        return [js.retain(fileEntry)];
       } else {
         return null;
       }
@@ -140,9 +155,12 @@ class ChromeFileSystem {
   /**
    * Returns the file entry with the given id if it can be restored. This call
    * will fail otherwise. This method is new in Chrome 30.
+   *
+   * This will return a dom FileEntry. js.retain() has been called on it; it is
+   * the caller's responsibility to call js.release();
    */
-  Future<FileEntry> restoreEntry(String id) {
-    ChromeCompleter<FileEntry> completer = new ChromeCompleter.oneArg();
+  Future<js.Proxy> restoreEntry(String id) {
+    ChromeCompleter<js.Proxy> completer = new ChromeCompleter.oneArg(js.retain);
     _fileSystem.restoreEntry(id, completer.callback);
     return completer.future;
   }
@@ -166,89 +184,13 @@ class ChromeFileSystem {
    * dev channel), entries are retained indefinitely. Otherwise, entries are
    * retained only while the app is running and across restarts. This method is
    * new in Chrome 30.
+   *
+   * [fileEntry] should be a dom FileEntry
    */
-  String retainEntry(FileEntry fileEntry) {
+  String retainEntry(js.Proxy fileEntry) {
     return _fileSystem.retainEntry(fileEntry);
   }
 }
-
-// FileEntry interface definition:
-//   http://www.w3.org/TR/file-system-api/#the-fileentry-interface
-//   http://dev.w3.org/2006/webapi/FileAPI/
-
-//interface Entry {
-//    readonly attribute boolean    isFile;
-//    readonly attribute boolean    isDirectory;
-//    void      getMetadata (MetadataCallback successCallback, optional ErrorCallback errorCallback);
-//    readonly attribute DOMString  name;
-//    readonly attribute DOMString  fullPath;
-//    readonly attribute FileSystem filesystem;
-//    void      moveTo (DirectoryEntry parent, optional DOMString newName, optional EntryCallback successCallback, optional ErrorCallback errorCallback);
-//    void      copyTo (DirectoryEntry parent, optional DOMString newName, optional EntryCallback successCallback, optional ErrorCallback errorCallback);
-//    DOMString toURL ();
-//    void      remove (VoidCallback successCallback, optional ErrorCallback errorCallback);
-//    void      getParent (EntryCallback successCallback, optional ErrorCallback errorCallback);
-//};
-
-/*
- * Utility methods for dealing with FileEntries (not strictly part of the chrome
- * app API).
- */
-
-/**
- * A conveinence method to read data from a dom [FileEntry]. The file contents
- * are returned as text.
- *
- * Errors will come back through the Future as [FileError].
- */
-Future<String> fileEntryRead(FileEntry fileEntry, [String encoding]) {
-  return fileEntry.file().then((File file) {
-    Completer<String> completer = new Completer();
-    FileReader reader = new FileReader();
-    reader.onLoadEnd.listen((_) => completer.complete(reader.result));
-    reader.onError.listen((_) => completer.completeError(reader.error));
-    if (encoding == null) {
-      reader.readAsText(file);
-    } else {
-      reader.readAsText(file, encoding);
-    }
-    return completer.future;
-  });
-}
-
-/**
- * A conveinence method to read data from a dom [FileEntry]. The file contents
- * are returned as binary (a list of ints).
- *
- * Errors will come back through the Future as [FileError].
- */
-Future<List<int>> fileEntryReadBinary(FileEntry fileEntry) {
-  return fileEntry.file().then((File file) {
-    Completer<List<int>> completer = new Completer();
-    FileReader reader = new FileReader();
-    reader.onLoadEnd.listen((_) => completer.complete(new Uint8List.view(reader.result)));
-    reader.onError.listen((_) => completer.completeError(reader.error));
-    reader.readAsArrayBuffer(file);
-    return completer.future;
-  });
-}
-
-/**
- * A conveinence method to write text to a dom [FileEntry]. On success, the same
- * [FileEntry] is returned from the Future.
- *
- * Errors will come back through the Future as [FileError].
- */
-Future<FileEntry> fileEntryWrite(FileEntry fileEntry, String text) {
-  return fileEntry.createWriter().then((FileWriter writer) {
-    Completer<FileEntry> completer = new Completer();
-    writer.onWriteEnd.listen((_) => completer.complete(fileEntry));
-    writer.onError.listen((_) => completer.completeError(writer.error));
-    writer.write(new Blob([text]));
-    return completer.future;
-  });
-}
-
 
 /**
  * For use in the [ChromeFileSystem.chooseEntry] and
@@ -286,3 +228,211 @@ class ChooseEntryAccepts implements js.Serializable {
     return js.map(m);
   }
 }
+
+/*
+ * Utility methods for dealing with FileEntries (not strictly part of the chrome
+ * app API).
+ */
+
+// FileEntry interface definition:
+//   http://www.w3.org/TR/file-system-api/#the-fileentry-interface
+//   http://dev.w3.org/2006/webapi/FileAPI/
+
+//interface Entry {
+//    readonly attribute boolean    isFile;
+//    readonly attribute boolean    isDirectory;
+//    void      getMetadata (MetadataCallback successCallback, optional ErrorCallback errorCallback);
+//    readonly attribute DOMString  name;
+//    readonly attribute DOMString  fullPath;
+//    readonly attribute FileSystem filesystem;
+//    void      moveTo (DirectoryEntry parent, optional DOMString newName, optional EntryCallback successCallback, optional ErrorCallback errorCallback);
+//    void      copyTo (DirectoryEntry parent, optional DOMString newName, optional EntryCallback successCallback, optional ErrorCallback errorCallback);
+//    DOMString toURL ();
+//    void      remove (VoidCallback successCallback, optional ErrorCallback errorCallback);
+//    void      getParent (EntryCallback successCallback, optional ErrorCallback errorCallback);
+//};
+
+/**
+ * The abstract parent of FileEntry and DirectoryEntry.
+ */
+abstract class Entry {
+  var _proxy;
+
+  Entry(this._proxy);
+
+  Entry.retain(this._proxy) {
+    js.retain(_proxy);
+  }
+
+  /**
+   * The name of the entry, excluding the path leading to it.
+   */
+  String get name => _proxy.name;
+
+  /**
+   * The full absolute path from the root to the entry.
+   */
+  String get fullPath => _proxy.fullPath;
+
+  /**
+   * Call [js.release] on the retained proxy.
+   */
+  void release() {
+    js.release(_proxy);
+  }
+
+  js.Proxy get proxy => _proxy;
+}
+
+/**
+ * A class to make working with js.Proxy instances that represent a dom
+ * FileEntry.
+ *
+ * see: http://www.w3.org/TR/file-system-api/
+ * see: http://dev.w3.org/2006/webapi/FileAPI/
+ */
+class FileEntry extends Entry {
+
+  /**
+   * Create a new FileEntry instance, given a [js.Proxy] reference to a dom
+   * FileEntry.
+   */
+  FileEntry(js.Proxy proxy): super(proxy);
+
+  /**
+   * Create a new FileEntry instance, given a [js.Proxy] reference to a dom
+   * FileEntry. Additionally, call [js.retain] on the proxy.
+   */
+  FileEntry.retain(js.Proxy proxy): super.retain(proxy);
+
+  // TODO: test reading with errors
+
+  /**
+   * Return the contents of the file as a String.
+   */
+  Future<String> readText() {
+    Completer<String> completer = new Completer();
+
+    js.Callback loadCallback = new js.Callback.once((var event) {
+      completer.complete(event.target.result);
+    });
+
+    js.Callback errorCallback = new js.Callback.once((var domError) {
+      completer.completeError(domError);
+    });
+
+    js.Callback fileCallback = new js.Callback.once((var file) {
+      var reader = new js.Proxy((js.context as dynamic).FileReader);
+      reader.onloadend = loadCallback;
+      reader.onerror = errorCallback;
+      reader.readAsText(file);
+    });
+
+    _proxy.file(fileCallback, errorCallback);
+
+    return completer.future;
+  }
+
+//  // TODO: implement
+//  /**
+//   * Return the contents of the file as binary.
+//   */
+//  Future<List<int>> readBinary() {
+//    Completer<String> completer = new Completer();
+//
+//    js.Callback loadCallback = new js.Callback.once((var event) {
+//      // TODO: event.target.result is an ArrayBuffer
+//
+//      completer.complete(event.target.result);
+//    });
+//
+//    js.Callback errorCallback = new js.Callback.once((var domError) {
+//      completer.completeError(domError);
+//    });
+//
+//    js.Callback fileCallback = new js.Callback.once((var file) {
+//      var reader = new js.Proxy((js.context as dynamic).FileReader);
+//      reader.onloadend = loadCallback;
+//      reader.onerror = errorCallback;
+//      reader.readAsArrayBuffer(file);
+//    });
+//
+//    _proxy.file(fileCallback, errorCallback);
+//
+//    return completer.future;
+//  }
+
+  /**
+   * Write out the given String to the file. On success, a references to [this]
+   * is returned.
+   */
+  Future<FileEntry> writeText(String text) {
+    Completer<FileEntry> completer = new Completer();
+
+    js.Callback writeEndCallback = new js.Callback.once((var event) {
+        completer.complete(this);
+    });
+
+    js.Callback errorCallback = new js.Callback.once((var event) {
+      completer.completeError(event);
+    });
+
+    js.Callback writerCallback = new js.Callback.once((var writer) {
+      // blob = new Blob([contents])
+      var blob = new js.Proxy((js.context as dynamic).Blob, js.array([text]));
+      writer.onwriteend = writeEndCallback;
+      writer.onerror = errorCallback;
+      writer.write(blob, js.map({'type': 'text/plain'}));
+    });
+
+//    TODO: the writeable entry stuff should be going away
+//    js.Callback writeableCallback = new js.Callback.once((var writeableEntry) {
+//      _proxy.createWriter(writerCallback, errorCallback);
+//    });
+//
+//    chromeProxy.fileSystem.getWritableEntry(_proxy, writeableCallback);
+    _proxy.createWriter(writerCallback, errorCallback);
+
+    return completer.future;
+  }
+}
+
+/**
+ * A class to make working with js.Proxy instances that represent a dom
+ * DirectoryEntry.
+ *
+ * see: http://www.w3.org/TR/file-system-api/
+ * see: http://dev.w3.org/2006/webapi/FileAPI/
+ */
+class DirectoryEntry extends Entry {
+
+  /**
+   * Create a new DirectoryEntry instance, given a [js.Proxy] reference to a dom
+   * DirectoryEntry.
+   */
+  DirectoryEntry(js.Proxy proxy): super(proxy);
+
+  /**
+   * Create a new DirectoryEntry instance, given a [js.Proxy] reference to a dom
+   * DirectoryEntry. Additionally, call [js.retain] on the proxy.
+   */
+  DirectoryEntry.retain(js.Proxy proxy): super.retain(proxy);
+
+}
+
+///**
+// * A convenience method to read data from a dom [FileEntry]. The file contents
+// * are returned as binary (a list of ints).
+// *
+// * Errors will come back through the Future as [FileError].
+// */
+//Future<List<int>> fileEntryReadBinary(FileEntry fileEntry) {
+//  return fileEntry.file().then((File file) {
+//    Completer<List<int>> completer = new Completer();
+//    FileReader reader = new FileReader();
+//    reader.onLoadEnd.listen((_) => completer.complete(new Uint8List.view(reader.result)));
+//    reader.onError.listen((_) => completer.completeError(reader.error));
+//    reader.readAsArrayBuffer(file);
+//    return completer.future;
+//  });
+//}
