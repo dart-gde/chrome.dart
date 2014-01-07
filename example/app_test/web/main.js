8744b81047f73ef3c4ad5d26e26a28dd53f87cfc
Adam Singer <financeCoding@gmail.com>
2014-01-06 20:55:50 -0800

    removing old files

--- example/app_test/web/main.js
+++
@@ -1,5 +0,0 @@
-
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('app_test.html',
-    {id: 'app_test_3', width: 800, height: 575});
-});
--- test/ext/background.js
+++
@@ -1,6 +0,0 @@
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.create({url: "harness_extension.html"}, function() {
-  	console.log("launching harness_extension.html");
-  });
-});
